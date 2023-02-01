b730e1a4d7d054d43531ee8605637875f8c517a1
Edvini-Code <121399785+Edvini-Code@users.noreply.github.com>
2023-02-01 22:44:34 +0200

    bookings

--- bookings.php
+++ bookings.php
@@ -1,30 +1,30 @@
 <?php
 
-include '../components/connect.php';
+include 'components/connect.php';
 
-if(isset($_COOKIE['admin_id'])){
-   $admin_id = $_COOKIE['admin_id'];
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
 }else{
-   $admin_id = '';
-   header('location:login.php');
+   setcookie('user_id', create_unique_id(), time() + 60*60*24*30, '/');
+   header('location:index.php');
 }
 
-if(isset($_POST['delete'])){
+if(isset($_POST['cancel'])){
 
-   $delete_id = $_POST['delete_id'];
-   $delete_id = filter_var($delete_id, FILTER_SANITIZE_STRING);
+   $booking_id = $_POST['booking_id'];
+   $booking_id = filter_var($booking_id, FILTER_SANITIZE_STRING);
 
-   $verify_delete = $conn->prepare("SELECT * FROM `bookings` WHERE booking_id = ?");
-   $verify_delete->execute([$delete_id]);
+   $verify_booking = $conn->prepare("SELECT * FROM `bookings` WHERE booking_id = ?");
+   $verify_booking->execute([$booking_id]);
 
-   if($verify_delete->rowCount() > 0){
-      $delete_bookings = $conn->prepare("DELETE FROM `bookings` WHERE booking_id = ?");
-      $delete_bookings->execute([$delete_id]);
-      $success_msg[] = 'Booking deleted!';
+   if($verify_booking->rowCount() > 0){
+      $delete_booking = $conn->prepare("DELETE FROM `bookings` WHERE booking_id = ?");
+      $delete_booking->execute([$booking_id]);
+      $success_msg[] = 'booking cancelled successfully!';
    }else{
-      $warning_msg[] = 'Booking deleted already!';
+      $warning_msg[] = 'booking cancelled already!';
    }
-
+   
 }
 
 ?>
@@ -35,67 +35,66 @@ if(isset($_POST['delete'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Bookings</title>
+   <title>bookings</title>
+
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="../css/admin_style.css">
+   <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
-   
-<!-- header section starts  -->
-<?php include '../components/admin_header.php'; ?>
-<!-- header section ends -->
 
-<!-- bookings section starts  -->
+<?php include 'components/user_header.php'; ?>
 
-<section class="grid">
+<!-- booking section starts  -->
 
-   <h1 class="heading">bookings</h1>
+<section class="bookings">
+
+   <h1 class="heading">my bookings</h1>
 
    <div class="box-container">
 
    <?php
-      $select_bookings = $conn->prepare("SELECT * FROM `bookings`");
-      $select_bookings->execute();
+      $select_bookings = $conn->prepare("SELECT * FROM `bookings` WHERE user_id = ?");
+      $select_bookings->execute([$user_id]);
       if($select_bookings->rowCount() > 0){
-         while($fetch_bookings = $select_bookings->fetch(PDO::FETCH_ASSOC)){
+         while($fetch_booking = $select_bookings->fetch(PDO::FETCH_ASSOC)){
    ?>
    <div class="box">
-      <p>booking id : <span><?= $fetch_bookings['booking_id']; ?></span></p>
-      <p>name : <span><?= $fetch_bookings['name']; ?></span></p>
-      <p>email : <span><?= $fetch_bookings['email']; ?></span></p>
-      <p>number : <span><?= $fetch_bookings['number']; ?></span></p>
-      <p>check in : <span><?= $fetch_bookings['check_in']; ?></span></p>
-      <p>check out : <span><?= $fetch_bookings['check_out']; ?></span></p>
-      <p>rooms : <span><?= $fetch_bookings['rooms']; ?></span></p>
-      <p>adults : <span><?= $fetch_bookings['adults']; ?></span></p>
-      <p>childs : <span><?= $fetch_bookings['childs']; ?></span></p>
+      <p>name : <span><?= $fetch_booking['name']; ?></span></p>
+      <p>email : <span><?= $fetch_booking['email']; ?></span></p>
+      <p>number : <span><?= $fetch_booking['number']; ?></span></p>
+      <p>check in : <span><?= $fetch_booking['check_in']; ?></span></p>
+      <p>check out : <span><?= $fetch_booking['check_out']; ?></span></p>
+      <p>rooms : <span><?= $fetch_booking['rooms']; ?></span></p>
+      <p>adults : <span><?= $fetch_booking['adults']; ?></span></p>
+      <p>childs : <span><?= $fetch_booking['childs']; ?></span></p>
+      <p>booking id : <span><?= $fetch_booking['booking_id']; ?></span></p>
       <form action="" method="POST">
-         <input type="hidden" name="delete_id" value="<?= $fetch_bookings['booking_id']; ?>">
-         <input type="submit" value="delete booking" onclick="return confirm('delete this booking?');" name="delete" class="btn">
+         <input type="hidden" name="booking_id" value="<?= $fetch_booking['booking_id']; ?>">
+         <input type="submit" value="cancel booking" name="cancel" class="btn" onclick="return confirm('cancel this booking?');">
       </form>
    </div>
    <?php
-      }
+    }
    }else{
-   ?>
+   ?>   
    <div class="box" style="text-align: center;">
-      <p>no bookings found!</p>
-      <a href="dashboard.php" class="btn">go to home</a>
+      <p style="padding-bottom: .5rem; text-transform:capitalize;">no bookings found!</p>
+      <a href="index.php#reservation" class="btn">book new</a>
    </div>
    <?php
-      }
+   }
    ?>
-
    </div>
 
 </section>
 
-<!-- bookings section ends -->
+<!-- booking section ends -->
 
 
 
@@ -111,13 +110,27 @@ if(isset($_POST['delete'])){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
 <!-- custom js file link  -->
-<script src="../js/admin_script.js"></script>
+<script src="js/script.js"></script>
 
-<?php include '../components/message.php'; ?>
+<?php include 'components/message.php'; ?>
 
 </body>
 </html>
