9d52e6a1dcc5423f6d146b88513cc31dbc9430bc
Edvini-Code <121399785+Edvini-Code@users.noreply.github.com>
2023-02-01 21:59:31 +0200

    Components

---
+++ admin_header.php
@@ -0,0 +1,23 @@
+<header class="header">
+    <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/admin_style.css">
+
+    <section class="flex">
+
+        <a href="dashboard.php" class="logo">AdminPanel.</a>
+
+        <nav class="navbar">
+            <a href="dashboard.php">home</a>
+            <a href="bookings">bookings</a>
+            <a href="admins.php">admins</a>
+            <a href="messages.php">messages</a>
+            <!--<a href="register.php">register</a>
+            <a href="login.php">login</a> -->
+            <a href="../components/admin_logout.php" onclick="return confirm('logout from this website?');">logout</a>
+        </nav>
+
+
+    </section>
+
+
+</header>
---
+++ footer.php
@@ -0,0 +1,39 @@
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="tel:38345909744"><i class="fas fa-phone"></i> +38345909744</a>
+         <a href="tel:38348417302"><i class="fas fa-phone"></i> +38348417302</a>
+         <a href="em58364@ubt-uni.net"><i class="fas fa-envelope"></i> em58364@ubt-uni.net</a>
+         <a href="nderimbunjakuu@gmail.com"><i class="fas fa-envelope"></i> nderimbunjakuu@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Kosovo, Pristina 10000</a>
+      </div>
+
+      <div class="box">
+         <a href="#home">home</a>
+         <a href="#about">about</a>
+         <a href="bookings.php">my bookings</a>
+         <a href="#reservation">reservation</a>
+         <a href="#gallery">gallery</a>
+         <a href="#contact">contact</a>
+         <a href="#reviews">reviews</a>
+      </div>
+
+      <div class="box">
+         <a href="#">facebook <i class="fab fa-facebook-f"></i></a>
+         <a href="#">twitter <i class="fab fa-twitter"></i></a>
+         <a href="#">instagram <i class="fab fa-instagram"></i></a>
+         <a href="#">linkedin <i class="fab fa-linkedin"></i></a>
+         <a href="#">youtube <i class="fab fa-youtube"></i></a>
+      </div>
+
+   </div>
+
+   <div class="credit">&copy; copyright @ 2022 by EDVINI - Code | all rights reseved!</div>
+
+</section>
+
+<!-- footer section ends -->
